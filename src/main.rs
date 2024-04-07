e83b2881d887ba99203531551fe637e7e3d52bdb
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-07 11:58:34 +0200

    feat: bind a port

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => {
+                println!("Accepted new connection")
+            }
+            Err(e) => {
+                println!("Error: {}", e)
+            }
+        }
+    }
 }
