7862e8def572210bd7661acbed523d8716272846
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-08 15:59:06 +0200

    fix: fix localhost ip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::net::SocketAddr;
+use std::{
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
 
 use anyhow::Result;
 use clap::Parser;
-use redis_starter_rust::{command::CommandRegistry, replication::ReplicaMode, stream::handle_stream};
+use redis_starter_rust::{
+    command::CommandRegistry, replication::ReplicaMode, stream::handle_stream,
+};
 use tokio::net::TcpListener;
 
 #[derive(Parser, Debug)]
@@ -19,13 +24,18 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await?;
     println!("Server listening on 127.0.0.1:{}", args.port);
 
-    let replica_mode = if let Some(replicaof) = args.replicaof {
-        let master_host = replicaof[0].clone();
-        let master_port: u16 = replicaof[1].parse().unwrap();
-        let master_addr = SocketAddr::new(master_host.parse().unwrap(), master_port);
-        ReplicaMode::Slave(master_addr)
-    } else {
-        ReplicaMode::Master
+    let mode = match &args.replicaof {
+        None => ReplicaMode::Master,
+        Some(args) => {
+            assert_eq!(args.len(), 2);
+            let addr = if args.first().unwrap() == "localhost" {
+                IpAddr::from_str("127.0.0.1").unwrap()
+            } else {
+                IpAddr::from_str(args.first().unwrap()).unwrap()
+            };
+            let port: u16 = args.get(1).unwrap().clone().parse().unwrap();
+            ReplicaMode::Slave(SocketAddr::new(addr, port))
+        }
     };
 
     let registry = CommandRegistry::new();
@@ -33,7 +43,7 @@ async fn main() -> Result<()> {
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
-                tokio::spawn(handle_stream(stream, registry.clone(), replica_mode.clone()));
+                tokio::spawn(handle_stream(stream, registry.clone(), mode.clone()));
             }
             Err(e) => {
                 eprintln!("Error accepting connection: {}", e);
