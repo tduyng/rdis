aed2244f0ac24bb43fb8867e331b0e9aa1502acd
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-08 17:48:06 +0200

    feat(replication): handle handshake PSYNC

--- src/replication/handshake.rs
+++ src/replication/handshake.rs
@@ -11,17 +11,32 @@ pub async fn perform_handshake(args: &[String]) -> Result<()> {
         .await
         .map_err(|e| anyhow!("Failed to connect to master: {}", e))?;
 
+    // Send PING command
     stream.write_all(b"*1\r\n$4\r\nping\r\n").await?;
 
+    // Receive response to PING
     let mut buf = Vec::from([0; 124]);
     let _ = stream.read(&mut buf).await;
+
+    // Send REPLCONF listening-port <PORT>
     stream
         .write_all(b"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n")
         .await?;
 
+    // Receive response to REPLCONF listening-port
     let _ = stream.read(&mut buf).await;
+
+    // Send REPLCONF capa eof capa psync2
     stream
-        .write_all(b"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+        .write_all(b"*5\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$3\r\neof\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+        .await?;
+
+    // Receive response to REPLCONF capa eof capa psync2
+    let _ = stream.read(&mut buf).await;
+
+    // Send PSYNC ? -1
+    stream
+        .write_all(b"*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
         .await?;
 
     Ok(())
